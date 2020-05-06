15cda6a8e7d52c8c8ffaf5062e198f9f36491e0f
Marvin Curlee <marvin.curle@gmail.com>
2020-05-05 21:04:25 -0500

    initial commit

---
+++ redirect.js
@@ -0,0 +1,17 @@
+// require http module
+const http = require('http');
+const port = 8080;
+
+// read REDIRECT_LOCATION var from env
+const location = process.env.REDIRECT_LOCATION;
+
+//create server and respond with 302
+const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location': location
+  });
+  res.end();
+});
+
+// start listener on port 8080
+server.listen(port, () => console.log(`Listening on port ${port}.....`));
